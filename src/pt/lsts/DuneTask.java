c8036cd93d32458afb209db519a8c5785a8b6b7c
Jose Pinto <zepinto@gmail.com>
2016-12-13 19:26:51 +0000

    All task inputs / outputs are now computed.

--- src/pt/lsts/DuneTask.java
+++ src/pt/lsts/DuneTask.java
@@ -1,10 +1,12 @@
 package pt.lsts;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -20,6 +22,7 @@ public class DuneTask {
 	DuneTask superTask;
 	HashSet<String> inputs = new HashSet<>();
 	HashSet<String> outputs = new HashSet<>();;
+	ArrayList<File> includedFiles = new ArrayList<>();
 
 	public DuneTask(File task) throws IOException {
 		this.filename = task;
@@ -29,20 +32,82 @@ public class DuneTask {
 		this.outputs = getOutputs(task);
 	}
 
+	public HashSet<String> inputs() {
+		HashSet<String> ret = new HashSet<>();
+		ret.addAll(inputs);
+		if (superTask != null)
+			ret.addAll(superTask.inputs());
+
+		return ret;
+	}
+
+	public HashSet<String> outputs() {
+		HashSet<String> ret = new HashSet<>();
+		ret.addAll(outputs);
+		if (superTask != null)
+			ret.addAll(superTask.outputs());
+
+		return ret;
+	}
+
+	private File getSrcDir() {
+		File f = filename;
+		while (!f.getName().equals("src"))
+			f = f.getParentFile();
+		return f;
+	}
+
+	private List<String> getCode(File f) throws IOException {
+		List<String> originalLines = Files.readAllLines(f.toPath());
+		ArrayList<String> lines = new ArrayList<>();
+
+		Iterator<String> it = originalLines.iterator();
+
+		while (it.hasNext()) {
+			String line = it.next();
+
+			if (line.startsWith("#include <") && line.endsWith("hpp>")) {
+				String l = line.substring("#include <".length());
+				l = l.substring(0, l.indexOf(">"));
+				File included = new File(getSrcDir(), l);
+				if (included.exists() && !includedFiles.contains(included)) {
+					includedFiles.add(included);
+					lines.addAll(getCode(included));
+				}
+			}
+			if (line.trim().endsWith(":")) {
+				line += " " + it.next();
+			}
+			lines.add(line);
+		}
+
+		return lines;
+	}
+
 	private String getType(File f) throws IOException {
-		List<String> lines = Files.readAllLines(f.toPath());
-		String superType = "DUNE::Tasks::Task";
-		for (String l : lines) {
+
+		String superType = "";
+		for (String l : getCode(f)) {
 			l = l.trim();
-			if (l.contains("struct Task:")) {
+			if (l.contains("struct Task")) {
+				if (l.contains(",")) {
+					l = l.substring(0, l.indexOf(","));
+				}
+				if (l.trim().endsWith(":")) {
+					superType = "Tasks::Task";
+				} else {
+					String[] parts = l.split(" ");
+					superType = parts[parts.length - 1];
+				}
+			} else if (l.contains("class") && l.contains("public")) {
 				String[] parts = l.split(" ");
-				superType = parts[parts.length-1];
+				superType = parts[parts.length - 1];
 			}
 		}
-		
+
 		return superType.replaceAll("DUNE::", "").replaceAll("::", ".");
 	}
-	
+
 	private String getName(File f) {
 		ArrayList<String> names = new ArrayList<>();
 
@@ -52,11 +117,14 @@ public class DuneTask {
 			dir = dir.getParentFile();
 		}
 		Collections.reverse(names);
-		return String.join(".", names);
+		if (!f.getName().equals("Task.cpp")) {
+			names.add(f.getName().replaceAll("\\.cpp", ""));
+		}
+		return String.join(".", names).replaceAll("DUNE.", "");
 	}
 
 	private HashSet<String> getInputs(File f) throws IOException {
-		List<String> lines = Files.readAllLines(f.toPath());
+		List<String> lines = getCode(f);
 		HashSet<String> inputs = new HashSet<>();
 
 		for (String l : lines) {
@@ -71,17 +139,16 @@ public class DuneTask {
 	}
 
 	private HashSet<String> getOutputs(File f) throws IOException {
-		List<String> lines = Files.readAllLines(f.toPath());
+		List<String> lines = getCode(f);
 		HashSet<String> outputs = new HashSet<>();
-
 		HashSet<String> dispatches = new HashSet<>();
-		
+
 		for (String l : lines) {
 			l = l.trim();
-
 			String prefix = "dispatch(";
+			l = l.replaceAll("Task::dispatch", "");
+			l = l.replaceAll("m_ctx\\.mbus\\.dispatch\\(", "");
 			if (l.contains(prefix)) {
-
 				l = l.substring(l.indexOf(prefix));
 				l = l.substring(prefix.length());
 
@@ -93,6 +160,13 @@ public class DuneTask {
 				l = l.replaceAll("&", "");
 				dispatches.add(l.trim());
 			}
+			else if (l.contains("dispatchReply(")) {
+				l = l.replaceAll("[\\*&\\(\\),;\\[\\]]+", " ");
+				l = l.replaceAll("DF_KEEP_SRC_EID", "");
+				String[] parts = l.split(" ");
+				dispatches.add(parts[parts.length-1].trim());
+			}
+ 			
 		}
 		Pattern p = Pattern.compile("IMC\\:\\:([\\w]*).*");
 
@@ -100,6 +174,8 @@ public class DuneTask {
 			l = l.trim();
 			if (l.startsWith("IMC::")) {
 				for (String v : dispatches) {
+					if (l.contains("iterator"))
+						continue;
 					if (l.contains(v)) {
 						Matcher m = p.matcher(l);
 						if (m.matches())
@@ -115,6 +191,11 @@ public class DuneTask {
 
 	@Override
 	public String toString() {
-		return name + " : "+superClass+" {\n\tinputs: " + inputs + "\n\toutputs: " + outputs + "\n}\n";
+		return name + " (" + filename + ") : " + superClass + " {\n\tinputs: " + inputs + "\n\toutputs: " + outputs
+				+ "\n}\n";
+	}
+	
+	public static void main(String[] args) throws Exception {
+		System.out.println(new DuneTask(new File("/home/zp/workspace/dune/source/src/DUNE/Tasks/Task.cpp")));
 	}
 }
--- src/pt/lsts/TaskListing.java
+++ src/pt/lsts/TaskListing.java
@@ -2,6 +2,7 @@ package pt.lsts;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 
 public class TaskListing {
@@ -20,6 +21,29 @@ public class TaskListing {
 			catch (Exception e) {
 				e.printStackTrace();
 			}			
+		}
+		
+		// Also process the inherited code (consumes / dispatches)
+		ArrayList<DuneTask> toProcess = new ArrayList<>();
+		toProcess.addAll(this.tasks.values());
+		
+		while (!toProcess.isEmpty()) {
+			ArrayList<DuneTask> curProc = new ArrayList<>();
+			curProc.addAll(toProcess);
+			for (DuneTask t : curProc) {
+				File f = resolveTaskFile(t.superClass);
+				if (f == null || this.tasks.containsKey(f)) {
+					toProcess.remove(t);
+					t.superTask = this.tasks.get(f);
+				}
+				else {
+					DuneTask parent = resolveTask(t.superClass);
+					this.tasks.put(f, parent);
+					toProcess.add(parent);
+					toProcess.remove(t);
+					t.superTask = parent;
+				}
+			}
 		}		
 	}
 	
@@ -39,12 +63,47 @@ public class TaskListing {
 		}
 	}
 	
-
+	private File resolveTaskFile(String taskName) {
+		HashSet<File> roots = new HashSet<>();
+		roots.add(new File(srcDir,"src"));
+		File core = new File(srcDir, "src/DUNE");
+		roots.add(core);
+		for (File f : core.listFiles()) { 
+			if (f.isDirectory())
+				roots.add(f);
+		}
+		
+		roots.add(new File(srcDir, "private/src"));
 	
+		String dir = taskName.replaceAll("\\.", "/");
+		
+		for (File r : roots) {
+			String fname = r.getAbsolutePath()+"/"+dir+".cpp";
+			if (new File(fname).exists()) {
+				return new File(fname);
+			}
+			fname = r.getAbsolutePath()+"/"+dir+"/Task.cpp";
+			if (new File(fname).exists()) {
+				return new File(fname);
+			}
+		}
+		
+		return null;
+	}
+	
+	private DuneTask resolveTask(String taskName) {
+		try {
+			return new DuneTask(resolveTaskFile(taskName));	
+		}
+		catch (Exception e) {
+			return null;
+		}
+	}
 	
 	public static void main(String[] args) throws IOException {
 		TaskListing tl = new TaskListing(new File("/home/zp/workspace/dune/source"));
-		System.out.println(tl.tasks);
-		
+		for (DuneTask t : tl.tasks.values()) {
+			System.out.println(t);
+		}
 	}
 }
